79814ad9b6754b5e51369cb860882d6131f5a376
John Antoni Griffiths <john@johnantoni.com>
2010-08-23 07:22:26 -0700

    fix

--- jquery.tamper.js
+++ jquery.tamper.js
@@ -13,15 +13,6 @@
  *  watchName   : element (textarea) to watch for changes
  *  exitName    : element (button) which classes as an exit when clicked
  *  messageText : text to show in the alert dialog
- *
- * Uses
- *  on input element of edit page, if user edits value, flag set and leaving via 
- *  back [exit] link show alert message
- *
- * Usage
- *  $.tamper('.backBtn', '.comment', 'Im sorry, Dave. Im afraid I cant do that.');
- *  $.tamperAlert('.backBtn', 'Did you really want to do that?');
- * 
  */
 
 jQuery.tamper = function(watchName, exitName, messageText) {
@@ -61,7 +52,7 @@ jQuery.tamper = function(watchName, exitName, messageText) {
 };
 
 // simpler version, basically assigns a message 
-// on-click to an exit element to open
+// on-click to an exit element to open if internal flag is true
 
 jQuery.tamperAlert = function(exitName, messageText) {
 
@@ -71,20 +62,35 @@ jQuery.tamperAlert = function(exitName, messageText) {
 
     // begin
     if (exitEl.length > 0) {
-
         // store arguments inside elements
         exitEl.data('tamper', { message: messageText});
-
+        
         // on clicking exit element, throw alert
         exitEl.click(function() {
             var returnVal = true;
             var msg = $(this).data('tamper').message;
+            var flag = $(this).data('altered') || false;
             
-            if (confirm(msg) === false) { 
-                returnVal = false; 
-            }
-            
+            if (flag) {
+                if (confirm(msg) === false) { 
+                    returnVal = false; 
+                }
+            }    
             return returnVal;
         });    
+    }
+};
+
+// simply flips the exit element's internal flag to true
+// useful when have less control over actions, e.g. inside iframes
+
+jQuery.tamperFlag = function(exitName) {
+
+    // init
+	var exitEl = $(exitName);
+
+    // begin
+    if (exitEl.length > 0) {
+        exitEl.data('altered', true);
     }
 };
